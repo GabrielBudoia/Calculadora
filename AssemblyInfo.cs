02c50f61f55ba8a31f4c62253fd566af63452c06
Gabriel Budoia <gabbudoia@gmail.com>
2026-03-09 19:55:22 +0100

    Update Assembly
    Added project description and technologies used for the Calculator App.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,10 +1,49 @@
-using System.Windows;
+<h1 align="center">Calculator App</h1>
+<p align="center">
+Aplicación de calculadora desarrollada en C# utilizando .NET<br/>
+</p>
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
-                                                //(used if a resource is not found in the page,
-                                                // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
-                                                //(used if a resource is not found in the page,
-                                                // app, or any theme specific resource dictionaries)
-)]
+<p align="center">
+  <a href="#-tecnologias">Tecnologías</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-proyecto">Proyecto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licencia">Licencia</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Calculator App" src="./public/Cover.png" width="100%">
+</p>
+
+## 🚀 Tecnologías
+
+Este proyecto fue desarrollado con las siguientes tecnologías:
+
+- C#
+- .NET
+- WPF
+- XAML
+- Git y GitHub
+
+## 💻 Proyecto
+
+Calculator App es una aplicación de escritorio que permite realizar operaciones matemáticas básicas de manera rápida y sencilla.
+
+Funcionalidades incluidas:
+
+- Suma
+- Resta
+- Multiplicación
+- División
+- Interfaz gráfica intuitiva
+
+Este proyecto tiene como objetivo **practicar el desarrollo de aplicaciones de escritorio con C# y .NET**, aplicando conceptos de interfaz gráfica, eventos y lógica de programación.
+
+## 🎨 Layout
+
+El diseño de la aplicación está enfocado en la **simplicidad y facilidad de uso**, proporcionando una interfaz clara para realizar cálculos de forma rápida y eficiente.
+
+## 📝 Licencia
+
+Este proyecto está bajo licencia MIT.
